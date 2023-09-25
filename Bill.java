e882d6b218c3d945a27fe40ec55090b478217dff
Lilit Hovhannisyan <117077817+LilitHovhannisyan7@users.noreply.github.com>
2023-09-25 23:47:29 +0400

    Update Bill.java

--- Bill.java
+++ Bill.java
@@ -9,29 +9,33 @@ public class Bill
     private double serviceFee;
     private double totalAmount;
 
+    private final double taxe = 0.2;
+    private final double fee = 0.1;
 
     public Bill(Room room, Customer customer, Booking booking)
     {
         this.room = room;
         this.customer = customer;
         this.booking = booking;
-        this.taxes = ChronoUnit.DAYS.between(booking.getStart(), booking.getEnd()) * 0.2 * (room.getPrice());
-        this.serviceFee = ChronoUnit.DAYS.between(booking.getStart(), booking.getEnd()) * 0.1 * (room.getPrice() + this.taxes);
+        this.taxes = ChronoUnit.DAYS.between(booking.getStart(), booking.getEnd()) * taxe * (room.getPrice());
+        this.serviceFee = ChronoUnit.DAYS.between(booking.getStart(), booking.getEnd()) * fee * (room.getPrice() + this.taxes);
         this.totalAmount = this.taxes + this.serviceFee + this.room.getPrice();
     }
 
 
     public void print()
     {
-        System.out.println("Customer Name: " + this.customer.getName());
-        System.out.println("Customer Email: " + this.customer.getContactEmail());
-        System.out.println("Room ID: " + this.room.getId());
-        System.out.println("Room Type: " + this.room.getType());
-        System.out.println("Booking Period: " + this.booking.getStart() + " to " + this.booking.getEnd());
-        System.out.println("Room Price: $" + this.room.getPrice());
-        System.out.println("Taxes (20%): $" + this.taxes);
-        System.out.println("Service Fee (10%): $" + this.serviceFee);
-        System.out.println("Total Amount: $" + this.totalAmount);
+        StringBuilder str = new StringBuilder("Customer Name: " + this.customer.getName() +
+                '\n' + "Customer Email: " + this.customer.getContactEmail() +
+                '\n' + "Room ID: " + this.room.getId() +
+                '\n' + "Room Type: " + this.room.getType() +
+                '\n' + "Booking Period: " + this.booking.getStart() + " to " + this.booking.getEnd() +
+                '\n' + "Room Price: $" + this.room.getPrice() +
+                '\n' + "Taxes (20%): $" + this.taxes +
+                '\n' + "Service Fee (10%): $" + this.serviceFee +
+                '\n' + "Total Amount: $" + this.totalAmount);
+
+        System.out.println(str);
     }
 
 
